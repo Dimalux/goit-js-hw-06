d4b14235b2e7fac7728be60a04b63c931b21631f
Dima Vujchich <astrallux777@ukr.net>
2025-05-03 18:53:02 +0300

    Initial commit

---
+++ js/task-4.js
@@ -0,0 +1,54 @@
+'use strict';
+
+const getTotalBalanceByGender = (users, gender) => {
+  const sums = users
+    .filter(user => user.gender === gender)
+    .map(sum => sum.balance)
+    .reduce((acum, number) => {
+      return acum + number;
+    }, 0);
+
+  return sums;
+};
+
+const clients = [
+  {
+    name: 'Moore Hensley',
+    gender: 'male',
+    balance: 2811,
+  },
+  {
+    name: 'Sharlene Bush',
+    gender: 'female',
+    balance: 3821,
+  },
+  {
+    name: 'Ross Vazquez',
+    gender: 'male',
+    balance: 3793,
+  },
+  {
+    name: 'Elma Head',
+    gender: 'female',
+    balance: 2278,
+  },
+  {
+    name: 'Carey Barr',
+    gender: 'male',
+    balance: 3951,
+  },
+  {
+    name: 'Blackburn Dotson',
+    gender: 'male',
+    balance: 1498,
+  },
+  {
+    name: 'Sheree Anthony',
+    gender: 'female',
+    balance: 2764,
+  },
+];
+
+console.log(getTotalBalanceByGender(clients, 'male')); // 12053
+
+console.log(getTotalBalanceByGender(clients, 'female')); // 8863
