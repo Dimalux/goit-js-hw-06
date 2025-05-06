5c77f8097060eca5309d21f426e6c57e18dfbbfd
Dima_V <astrallux777@ukr.net>
2025-05-06 22:30:28 +0300

    06.05.2025-DZ-06-02

--- js/task-2.js
+++ js/task-2.js
@@ -1,37 +1,33 @@
 'use strict';
 
-
-
 class Storage {
-    items
+  #items;
 
-    getItems() {
+  constructor(items) {
+    this.#items = items;
+  }
 
-    }
+  getItems() {
+    return this.#items;
+  }
 
-    addItem(newItem) {
+  addItem(newItem) {
+    this.#items.push(newItem);
+  }
 
-    }
-
-    removeItem(itemToRemove) {
-
-    }
-
-
-
+  removeItem(itemToRemove) {
+    this.#items = this.#items.filter(item => item !== itemToRemove);
+  }
 }
 
-
-
-
-const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
+const storage = new Storage(['Nanitoids', 'Prolonger', 'Antigravitator']);
 console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
 
-storage.addItem("Droid");
+storage.addItem('Droid');
 console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
 
-storage.removeItem("Prolonger");
+storage.removeItem('Prolonger');
 console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
 
-storage.removeItem("Scaner");
+storage.removeItem('Scaner');
 console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
